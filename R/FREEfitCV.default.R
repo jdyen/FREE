293403965ebbc0be6904bc387d22faf5ac0fb8b9
jdyen <jdl.yen@gmail.com>
2014-03-03 13:46:31 +1100

    Add warning for incorrect method

--- R/FREEfitCV.default.R
+++ R/FREEfitCV.default.R
@@ -3,10 +3,14 @@ function(y, x, bins=1:ncol(y), method=c("fda", "gamboost", "INLA", "stan", "BUGS
   n.obs <- nrow(y)
   observed <- NULL
   predicted <- NULL
+  if (!any(method == c("fda", "gamboost", "INLA", "stan", "BUGSspline", "BUGSjump", "FREE"))) {
+    warning("Method must be one of fda, gamboost, INLA, stan, BUGSspline, BUGSjump or FREE. fda method selected by default...", call.=FALSE)
+    method <- "fda"
+  }
   if (method == "stan") {
     cv.temp <- list()
-    cv.temp.stan <- FREEcvInternal(i=1, n.obs=n.obs, n.cv=n.cv, y=y, x=x, bins=bins, method=method, verbose=verbose,
-                                   ...)
+    cv.temp.stan <- FREEcvInternal(i=1, n.obs=n.obs, n.cv=n.cv, y=y, x=x, bins=bins,
+                                   method=method, verbose=verbose, ...)
     cv.temp[[1]] <- list(observed=cv.temp.stan$observed, predicted=cv.temp.stan$predicted)
     stan.model <- cv.temp.stan$stan.model
     cv.temp2 <- lapply(c(2:n.cv), FREEcvInternal, n.obs=n.obs, n.cv=n.cv, y=y, x=x, bins=bins,
