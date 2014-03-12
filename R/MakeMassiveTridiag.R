3f676102513434c57d369d441e0988360963d5e7
jdyen <jdl.yen@gmail.com>
2014-03-12 12:19:21 +1100

    Fix memory use in massive tridiag

--- R/MakeMassiveTridiag.R
+++ R/MakeMassiveTridiag.R
@@ -1,13 +1,15 @@
 MakeMassiveTridiag <-
 function(tridiag, n.times){
-  mat0 <- matrix(0, nrow=nrow(tridiag), ncol=ncol(tridiag))
   mat.temp <- lapply(1:n.times, 
-                     function(i, mat0, tridiag){
-                       mat.out <- rbind(do.call("rbind", rep(list(mat0), {i - 1})),
-                       tridiag, do.call("rbind", rep(list(mat0), {n.times - i})))
+                     function(i, tridiag, n.times){
+                       mat.out <- matrix(0, nrow={nrow(tridiag) * n.times},
+                                         ncol=ncol(tridiag))
+                       mat.out[{{i - 1} * nrow(tridiag) + 1}:{i * nrow(tridiag)},
+                                 1:ncol(tridiag)] <-
+                                 tridiag
                        return(mat.out)
                      },
-                     mat0=mat0, tridiag=tridiag)
+                     tridiag=tridiag, n.times=n.times)
   mat <- do.call("cbind", mat.temp)
   mat
 }
