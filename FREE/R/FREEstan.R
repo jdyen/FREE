34c37360dc0dfd1507f82c94e2fff652f72ac68a
Jian Yen <jdyen@Jians-MacBook-Pro.local>
2015-10-15 12:03:55 +1100

    update stan imports

--- FREE/R/FREEstan.R
+++ FREE/R/FREEstan.R
@@ -1,8 +1,5 @@
 FREEstan <-
 function(y, x, bins, family="gaussian", errors="ar1", n.basis=12, stan.file=NULL, n.chains=3, n.iters=2000, n.burnin=n.iters/2, n.thin=1, verbose=FALSE, refresh=max(n.iters/10, 1), stan.model=NULL, ...){
-  if (get_cppo()$mode != "fast") {
-    set_cppo("fast")
-  }
   if (is.null(stan.file) & is.null(stan.model)) {
     stan.file <- stanCodeDefault()
   }
